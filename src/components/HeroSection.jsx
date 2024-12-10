23ed50619c363e010d6c65d9c7ea069bcdbc3253
XalilovaSM <Surayya.Xalilova@kapitalbank.az>
2024-12-10 16:01:37 +0400

    Change some texts

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ export const HeroSection = () => {
   return (
     <div className="flex flex-col items-center mt-6 lg:mt-20">
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide dark:text-white">
-        VirtualVR build tools
+        Free online courses
         <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
           {" "}
           for developers
